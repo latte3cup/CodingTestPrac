f7c163a73ef51faa5e9c3096f16d5fbcfd906baf
JunGeol <118102681+latte3cup@users.noreply.github.com>
2025-03-19 14:44:41 +0900

    [level 2] Title: 소수 찾기, Time: 5.80 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,6 +1,4 @@
 def is_prime(num):
-    if num ==0 or num ==1:
-        return 0
     for i in range(2, int(num**0.5) + 1):
         if num%i == 0: 
             return 0
@@ -9,7 +7,7 @@ def is_prime(num):
 def solution(numbers):
     ans = 0
     from itertools import permutations
-    l = {int("".join(x)) for y in range(1,len(numbers)+1) for x in list(permutations(list(numbers), y)) }
+    l = {int("".join(x)) for y in range(1,len(numbers)+1) for x in list(permutations(list(numbers), y)) } - {0,1}
     for n in l:
         ans = ans+ is_prime(n)
         
