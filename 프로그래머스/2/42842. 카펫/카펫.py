dc5de262957b65a2315b24d400bd1ab831270a57
JunGeol <118102681+latte3cup@users.noreply.github.com>
2025-03-19 17:38:09 +0900

    [level 2] Title: 카펫, Time: 0.16 ms, Memory: 9.31 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,9 @@
+def solution(brown, yellow):
+    s = brown+ yellow
+    for i in range(3,int(s**0.5) +1):
+        if s%i !=0: continue
+        elif (i-2)*((s//i) -2) == yellow:
+            return sorted([i,(s//i)], reverse=True)
+        print(f"{i}//{s//i}")
+    
+    return 1
