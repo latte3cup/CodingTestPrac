04efe79ecb6c50b9e9382864477b84d56af0377f
JunGeol <118102681+latte3cup@users.noreply.github.com>
2025-04-04 20:44:02 +0900

    [level 2] Title: 큰 수 만들기, Time: 193.46 ms, Memory: 15.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,44 @@
+def solution2(number, k):
+    for i in range(k):
+        ans=0
+        l = len(number)
+        for j in range(l):
+            check = number[:j] + number[j+1:]
+            ans = max(int(check),ans)
+        number = str(ans)
+
+    return str(ans)
+
+
+def solution(number,k):
+    number = list(number)
+    stack = []
+    cur = 0
+    while k!=0 and cur < len(number):
+        if not stack:
+            stack.append(number[cur])
+            cur+=1
+        elif stack and stack[-1] >= number[cur]:
+            stack.append(number[cur])
+            cur+=1
+        elif stack and stack[-1] < number[cur]:
+            stack.pop()
+            k-=1
+            continue
+            
+        
+            
+
+        
+        
+        
+    for i in range(cur,len(number)):
+        stack.append(number[i])
+    
+    for j in range(k):
+        stack.pop()
+    
+    return ''.join(stack)
+
+
+        
