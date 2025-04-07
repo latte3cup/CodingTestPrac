cc37b1e8c13a5afa61158d9350780826cf2a4fb4
JunGeol <118102681+latte3cup@users.noreply.github.com>
2025-04-08 01:00:01 +0900

    [level 3] Title: 단속카메라, Time: 0.86 ms, Memory: 9.58 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,8 +1,8 @@
 def solution(routes):
     routes.sort(key=lambda x: x[1])  # 종료 지점 기준으로 정렬
     cnt = 0
-    camera = -30001
-
+    camera = routes[0][0] -1
+    
     for start, end in routes:
         if camera < start:
             cnt += 1
