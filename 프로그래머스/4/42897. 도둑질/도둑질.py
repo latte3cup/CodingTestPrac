c904c1e12b8527cdf9781b3898ca79ff11f6242f
JunGeol <118102681+latte3cup@users.noreply.github.com>
2025-04-21 19:20:20 +0900

    [level 4] Title: 도둑질, Time: 2314.91 ms, Memory: 493 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,24 +1,17 @@
 import sys
 sys.setrecursionlimit(10**7)
 
-def dfs(i, money, memo):
+def dp(i, money, memo):
     if i >= len(money):
         return 0
     if i in memo:
         return memo[i]
     
-    # 현재 집을 턴다고 하면
-    take = money[i] + max(dfs(i + 2, money, memo), dfs(i + 3, money, memo))
-
-    # 현재 집을 안 턴다고 하면
-    skip = dfs(i + 1, money, memo)
+    take = money[i] + max(dp(i + 2, money, memo), dp(i + 3, money, memo))
+    skip = dp(i + 1, money, memo)
 
     memo[i] = max(take, skip)
     return memo[i]
 
 def solution(money):
-    # 원형 처리: case 1 (0 ~ n-2), case 2 (1 ~ n-1)
-    return max(
-        dfs(0, money[:-1], {}),  # 첫 집 포함, 마지막 제외
-        dfs(0, money[1:], {})   # 첫 집 제외, 마지막 포함
-    )
+    return max(dp(0, money[:-1], {}),dp(0, money[1:], {}))
